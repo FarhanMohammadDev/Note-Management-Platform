7687e8684015720b6680eba72dd280257e967424
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-02-29 10:37:59 +0100

    implementation redux

--- src/lib/features/notes/notesSlice.ts
+++ src/lib/features/notes/notesSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface Note {
+    id ?: string;
     title: string;
     content: string;
     status: string;
@@ -26,6 +27,104 @@ export const fetchNotesThunk = createAsyncThunk(
     }
 );
 
+// Création d'une fonction thunk pour ajouter une nouvelle note
+export const addNoteThunk = createAsyncThunk(
+    'notes/addNote',
+    async (newNote: Note) => {
+        try {
+            const response = await fetch('http://localhost:3000/api/notes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newNote),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to add note');
+            }
+
+            const data = await response.json();
+            return data; 
+        } catch (error:any) {
+            throw new Error(error.message);
+        }
+    }
+);
+
+// by id
+
+// Thunk pour récupérer une note par son ID
+export const getOneNoteThunk = createAsyncThunk(
+    'notes/getOneNote',
+    async (noteId: string) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/notes/${noteId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to get note'); 
+            }
+
+            const data = await response.json(); 
+            return data; 
+        } catch (error : any) {
+            throw new Error(error.message); 
+        }
+    }
+);
+
+// Thunk pour mettre à jour une note
+export const updateNoteThunk = createAsyncThunk(
+    'notes/updateNote',
+    async (updatedNote: Note) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/notes/${updatedNote.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedNote), 
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to update note'); 
+            }
+
+            const data = await response.json(); 
+            return data; 
+        } catch (error : any) {
+            throw new Error(error.message); 
+        }
+    }
+);
+
+// Thunk pour supprimer une note
+export const deleteNoteThunk = createAsyncThunk(
+    'notes/deleteNote',
+    async (noteId: string) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/notes/${noteId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to delete note'); 
+            }
+
+            return noteId; 
+        } catch (error : any) {
+            throw new Error(error.message); 
+        }
+    }
+);
 
 
 // -------------------------------------------------------------------------------------
@@ -48,7 +147,6 @@ const initialState= {
       // fulfilled
       builder.addCase(fetchNotesThunk.fulfilled, (state, action) => {
         console.log("fetchNotesThunk",action.payload);
-        
         state.isLoading = false;
         state.notes = action.payload;
       });
